8388d42e83a533c4804ba3af613b47524893afe6
flipwhatthezip2 <flipwhatthezip2.mc@gmail.com>
2015-05-21 21:04:44 -0400

    * Added chat formatting.

---
+++ src/minelegends/minigameAPI/Core.java
@@ -0,0 +1,28 @@
+package minelegends.minigameAPI;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/*
+
+------------------------
+    minigameAPI
+------------------------
+ Created by: cfletcher
+ Created on: 5/21/15
+------------------------
+All code below is owned by
+Cameron Fletcher and is not
+to be edited/ copied without
+my (Cameron Fletcher)'s permission.
+
+*/
+public class Core extends JavaPlugin {
+
+    public static String join = "§6§lJOIN> §7";
+    public static String quit = "§6§lQUIT> §7";
+    public static String death = "§6§lDEATH> §7";
+    public static String team = "§6§lTEAM> §7";
+    public static String status = "§6§lSTATUS> §7";
+    public static String notice = "§6§lNOTICE> §7";
+
+}
